dc487ec3c1075922a7b2432a05fdf9498e238def
heyimkko <enhasa@gmail.com>
2015-05-31 15:41:18 -0700

    Refactor Game component

--- js/Game.js
+++ js/Game.js
@@ -1,7 +1,7 @@
 var Game = React.createClass({
   getInitialState: function() {
     return {
-      gridMarks: ['', '', '', '', '', '', '', '', ''],
+      grid: ['', '', '', '', '', '', '', '', ''],
       turn: 0,
       previousPlayerMoves: [],
       playerMark: this.props.playerMark,
@@ -18,7 +18,7 @@ var Game = React.createClass({
       }
 
       if (!this.state.isHumanTurn) {
-        this.initializeTurn();
+        this.initializeAITurn();
 
         this.setState({
           isHumanTurn: !this.state.isHumanTurn,
@@ -36,35 +36,36 @@ var Game = React.createClass({
 
   updateGrid: function(index) {
     if (this.state.isHumanTurn && !this.state.gameOver) {
-      var newGrid = this.state.gridMarks.slice();
+      var newGrid = this.state.grid.slice();
       var movesArray = this.state.previousPlayerMoves;
 
       newGrid[index] = this.state.playerMark;
       movesArray.push(index);
 
       this.setState({
-        gridMarks: newGrid,
+        grid: newGrid,
         isHumanTurn: !this.state.isHumanTurn,
         turn: this.state.turn + 1,
         previousPlayerMoves: movesArray
-      })
+      });
     } else {
       return false;
+
     }
   },
 
   checkWinCondition: function() {
     var winningLane = GridHelper.allLanes().find(function(cells) {
-      var markCheck = this.state.gridMarks[cells[0]];
+      var markCheck = this.state.grid[cells[0]];
 
-      return (markCheck && this.state.gridMarks[cells[1]] === markCheck && this.state.gridMarks[cells[2]] === markCheck)
+      return (markCheck && this.state.grid[cells[1]] === markCheck && this.state.grid[cells[2]] === markCheck);
     }.bind(this));
 
     if (winningLane) {
-      this.endGame(this.state.gridMarks[winningLane[0]]);
+      this.endGame(this.state.grid[winningLane[0]]);
 
-    } else if (!winningLane && this.state.turn === this.state.gridMarks.length) {
-      this.endGame();
+    } else if (!winningLane && this.state.turn === this.state.grid.length) {
+      this.endGame(); // draw
 
     } else {
       return false;
@@ -73,43 +74,42 @@ var Game = React.createClass({
   },
 
   endGame: function(mark) {
+    var customTitle;
+    var customText;
+    var customType;
     var resetMessage = " Click Reset to play again.";
-    var options = {
-      confirmButtonText: "Ok",
-      confirmButtonColor: "#81D4FA",
-      allowOutsideClick: true
-    };
+    var options = {confirmButtonText: "Ok", confirmButtonColor: "#81D4FA", allowOutsideClick: true};
 
     if (mark === aiMark) {
-      options["title"] = "You Lose!";
-      options["text"] = "Game Theory Optimal Bot can't be beaten!" + resetMessage;
-      options["type"] = "error";
-
-      swal(options);
+      customTitle = "You Lose!";
+      customText = "Game Theory Optimal Bot can't be beaten!";
+      customType = "error";
 
     } else if (mark === playerMark) {
-      options["title"] = "You Win!";
-      options["text"] = "You won! Nice Hax." + resetMessage;
-      options["type"] = "success";
-
-      swal(options);
+      customTitle = "You Win!";
+      customText = "You won! Nice Hax.";
+      customType = "success";
 
     } else if (!mark) {
-      options["title"] = "Draw!";
-      options["text"] = "Is that the best you can do?" + resetMessage;
-
-      swal(options);
+      customTitle = "Draw!";
+      customText = "Is that the best you can do?";
 
     }
+
+    options["title"] = customTitle;
+    options["text"] = customText + resetMessage;
+    options["type"] = customType;
+
+    swal(options);
 
     this.setState({gameOver: true});
   },
 
-  initializeTurn: function() {
+  initializeAITurn: function() {
     var markIndex = '';
     var winAttempt = this.planMove(this.state.aiMark, this.state.playerMark);
     var preventDefeat = this.planMove(this.state.playerMark, this.state.aiMark);
-    var newGrid = this.state.gridMarks.slice();
+    var newGrid = this.state.grid.slice();
 
     if (winAttempt) { // Attempt to win (or take center cell) if possible
       markIndex = winAttempt;
@@ -118,51 +118,44 @@ var Game = React.createClass({
       markIndex = preventDefeat;
 
     } else { // Take a blocking corner or any remaining cell
-      markIndex = this.aiRespondWithClosest();
+      markIndex = this.nextOptimalAIResponse();
 
     }
 
     newGrid[markIndex] = this.state.aiMark;
 
-    this.setState({gridMarks: newGrid});
+    this.setState({grid: newGrid});
   },
 
   planMove: function(markerToScan, markerToIgnore) {
-    var unmarkedIndices = [];
+    var unmarkedIndex;
     var centerCell = 4;
     var lanesToParse = GridHelper.allLanes();
 
-    if (this.state.turn === 1 && this.state.gridMarks[centerCell] === '') {
-      return centerCell;
+    if (this.state.turn === 1 && this.state.grid[centerCell] === '') {
+      return centerCell; // take center cell if available
     }
 
     var lane = lanesToParse.find(function(cells) {
       var counter = 0;
 
       for (var i = 0; len = cells.length, i < len; i++) {
-        if (this.state.gridMarks[cells[i]] === markerToIgnore) {
+        if (this.state.grid[cells[i]] === markerToIgnore) {
           counter--;
 
-        } else if (this.state.gridMarks[cells[i]] === markerToScan) {
+        } else if (this.state.grid[cells[i]] === markerToScan) {
           counter++;
 
         } else {
-          unmarkedIndices.push(cells[i]);
+          unmarkedIndex = cells[i]; // last unmarked index if lane is found will be AI's optimal placement
 
         }
       }
 
-      return counter === 2
+      return counter === 2;
     }.bind(this));
 
-    // Attempt an advantageous action if possible
-    if (lane && unmarkedIndices) {
-      return unmarkedIndices.find(function(cell) {
-        return lane.indexOf(cell) > -1;
-      });
-    } else {
-      return false;
-    }
+    return (lane && unmarkedIndex || false); // Return advantageous placement if available
   },
 
   // If there is no win or defeat condition available, AI needs to figure out the next best move
@@ -170,37 +163,32 @@ var Game = React.createClass({
   // 1. Prevent potential forks on turn 3 by taking a blocking corner
   // 2. Take a corner
   // 3. Take anything left available (the game is destined for a draw by this point)
-  aiRespondWithClosest: function() {
+  nextOptimalAIResponse: function() {
     var previousPlayerMoves = this.state.previousPlayerMoves.slice(-2);
     var secondToLastMove = previousPlayerMoves[0];
     var lastMove = previousPlayerMoves[1];
     var diff = Math.abs(lastMove - secondToLastMove);
     var cornerCells = GridHelper.cornerCells();
-    var takeOptimalCell;
+    var optimalCell;
     var forkConditionsPossible = this.state.turn === 3 && secondToLastMove !== 4 && (diff >= 4 && diff <= 8);
 
     if (forkConditionsPossible) { // Prevent fork threats
-      takeOptimalCell = GridHelper.findOptimalBlockingCell(lastMove, secondToLastMove);
+      optimalCell = GridHelper.findOptimalBlockingCell(lastMove, secondToLastMove);
 
     } else if (lastMove < 4) { // Otherwise choose an open corner
-      takeOptimalCell = cornerCells.slice(0, 2).find(this.isCellAvailable);
+      optimalCell = cornerCells.slice(0, 2).find(this.isCellAvailable);
 
     } else {
-      takeOptimalCell = cornerCells.slice(2).find(this.isCellAvailable);
+      optimalCell = cornerCells.slice(2).find(this.isCellAvailable);
 
     }
 
-    if (takeOptimalCell > -1) { // Favor corner cells
-      return takeOptimalCell;
-
-    } else { // Otherwise fall back to any cell
-      return this.state.gridMarks.findIndex(this.isCellEmpty);
-
-    }
+    // favor any optimal corner cells, otherwise fall back to any empty cell
+    return (optimalCell > -1 && optimalCell) || this.state.grid.findIndex(this.isCellEmpty);
   },
 
   isCellAvailable: function(cell) {
-    var cellValue = this.state.gridMarks[cell];
+    var cellValue = this.state.grid[cell];
 
     return (cellValue === '' || !cellValue === this.state.playerMark);
   },
@@ -212,8 +200,8 @@ var Game = React.createClass({
   render: function() {
     var grid = []
 
-    for (var i = 0; len = this.state.gridMarks.length, i < len; i++) {
-      grid.push(<Cell key={i} index={i} mark={this.state.gridMarks[i]} handleClick={this.updateGrid} />)
+    for (var i = 0; len = this.state.grid.length, i < len; i++) {
+      grid.push(<Cell key={i} index={i} mark={this.state.grid[i]} handleClick={this.updateGrid} />);
     }
 
     return (
